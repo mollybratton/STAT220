8c82532725306c3f1e39a16e60e118ed07dc4407
mollybratton <mollyjbratton@gmail.com>
2025-03-18 13:52:04 -0500

    Delete beast-mode directory

--- beast-mode/ideas.R
+++
@@ -1,8 +0,0 @@
-#         ui <- page_navbar(
-#           theme = bs_theme(version = 5, bootswatch = "minty"),
-#           tabPanel("Introduction", p("An app for analyzing Starwars data."),
-#           tabPanel("Data", gt: gt_output("dataTable")),
-#           navbarMenu("More Analyses", 
-#               tabPanel("Height Analysis", plotOutput ("heightplot")),
-#               tabPanel("Mass Analysis", plotOutput ("massPlot"))
-#         )
